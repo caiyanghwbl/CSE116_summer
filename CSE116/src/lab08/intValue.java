f507bef2797bb3a421600c38aee8e654ca43c67b
caiyang <caiyang@1macbook-pro>
2017-08-07 11:19:58 -0400

    Start Lab08

---
+++ CSE116/src/lab08/intValue.java
@@ -0,0 +1,25 @@
+package lab08;
+
+public class intValue {
+	String value = "";
+	
+	public intValue(String value) {
+		value = this.value;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+	
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	public intValue add(intValue n) {
+		String num1 = getValue();
+		String num2 = n.getValue();
+		
+		return null;
+		
+	}
+}
