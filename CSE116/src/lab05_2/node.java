c6d7a2c4e43caf151e7d8d0b6744dc4f8942d429
caiyang <caiyang@1macbook-pro>
2017-08-07 11:43:04 -0400

    Done with node(not tested yet)

--- CSE116/src/lab05_2/node.java
+++ CSE116/src/lab05_2/node.java
@@ -1,5 +1,42 @@
 package lab05_2;
 
 public class node {
-
+	node left, right;
+	int value;
+	
+	public node() {
+		left = null;
+		right = null;
+		value = 0;
+	}
+	
+	public node(int value) {
+		left = null;
+		right = null;
+		this.value = value;
+	}
+	
+	public void setLeftNode(node leftValue) {
+		left = leftValue;
+	}
+	
+	public void setRightNode(node rightValue) {
+		right = rightValue;
+	}
+	
+	public node getLeftNode() {
+		return left;
+	}
+	
+	public node getRightNode() {
+		return right;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return this.value;
+	}
 }
