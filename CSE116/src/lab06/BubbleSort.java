b7390a0d7eacf6fb19d785fc5d4ec5306cf17d20
caiyanghwbl <yangcai@buffalo.edu>
2017-08-09 12:10:24 -0400

    Done with BubbleSort

--- CSE116/src/lab06/BubbleSort.java
+++ CSE116/src/lab06/BubbleSort.java
@@ -2,4 +2,35 @@ package lab06;
 
 public class BubbleSort {
 
+	public static void sort(double[] arrayToBeSorted) {
+		double temp = 0;
+		int position = 0;
+		for (int i = 0; i < arrayToBeSorted.length; i++) {
+			position = i;
+			temp = arrayToBeSorted[i];
+			
+			for (int j = i + 1; j < arrayToBeSorted.length; j++) {
+				if (temp > arrayToBeSorted[j]){//交换两个数位置
+					temp = arrayToBeSorted[j];
+					position = j;
+				}
+			}
+			arrayToBeSorted[position] = arrayToBeSorted[i];
+			arrayToBeSorted[i] = temp;
+		}
+	}
+	
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) {
+		double ArrayToBeSorted[] = new double[100000];
+		for (int i = 0; i < 100000; ++i) {
+			ArrayToBeSorted[i] = Math.random();
+		}
+		BubbleSort bs = new BubbleSort();
+		long time1 = System.nanoTime();
+		bs.sort(ArrayToBeSorted);
+		long elapsed = System.nanoTime() - time1;
+		long result = elapsed / 100000000;
+		System.out.println("Elapsed time: " + result + " milliseconds");
+	}
 }
